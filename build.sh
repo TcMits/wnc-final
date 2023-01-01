c9db86353ed89ab98fe825343319b75a7190ec56
hyuti <lehuy.hl27@gmail.com>
2023-01-01 18:39:24 +0700

    update script

--- build.sh
+++ build.sh
@@ -3,3 +3,6 @@
 set -o errexit
 
 go mod tidy
+go build -tags netgo -ldflags '-s -w' -o createuser ./cmd/createuser
+go build -tags netgo -ldflags '-s -w' -o migrate ./cmd/migrate
+go build -tags netgo -ldflags '-s -w' -o app
